57b7810db009b4e303a27a72fe87e49825ff0595
YarynaPas <pas06chna@gmail.com>
2024-02-23 18:14:02 +0200

    Initial commit

---
+++ lesson1/hw1.js
@@ -0,0 +1,44 @@
+let a = 'hello';
+let b = 'owu';
+let c = 'com';
+let d = 'ua';
+let x = 1;
+let y = 10;
+let z = -999;
+let q = 123;
+let e = 3.14;
+let p = 2.7;
+let o = 16;
+let k = true;
+let j = false;
+console.log()
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(x);
+console.log(y);
+console.log(z);
+console.log(q);
+console.log(e);
+console.log(p);
+console.log(o);
+console.log(k);
+console.log(j);
+let firstName='Ярина';
+let middleName='Марія';
+let lastName='Пасічна';
+let sen=`${firstName} ${middleName} ${lastName}`;
+console.log(sen);
+let a1 = 100;
+let b1 = '100';
+let c1 = true;
+console.log(typeof a1);
+console.log(typeof b1);
+console.log(typeof c1);
+
+
+
+
+
+
