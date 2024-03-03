0e82605678e1b2dfbaf21def320fd968582b80be
YarynaPas <pas06chna@gmail.com>
2024-03-04 01:01:14 +0200

    hw2

--- hw5/hw5.js
+++ hw5/hw5.js
@@ -1,66 +1,58 @@
 //- створити функцію яка обчислює та повертає площу прямокутника зі сторонами а і б
-function area1(a,b) {
-    return a*b;
+const area1 = (a, b) => a * b;
+
+const a = parseInt(prompt('Введіть 1 сторону:'));
+const b = parseInt(prompt('Введіть 2 сторону:'));
+
+const area = area1(a, b);
+
+if (area > 0) {
+    console.log(`Площа прямокутника зі сторонами ${a} та ${b} = ${area}`);
+} else {
+    console.log(`Площу прямокутника зі сторонами ${a} та ${b} неможливо обрахувати`);
 }
-a= parseInt(prompt('Введіть 1 сторону:'));
-b= parseInt(prompt('Введіть 2 сторону:'));
-let area = area1(a, b);
-if (area>0){
-    console.log(`Площа прямокутника зі сторонами ${a} та ${b} = ${area}`)
-}
-else {
-    console.log(`Площу прямокутника зі сторонами ${a} та ${b} неможливо обрахувати`)
-}
-    //- створити функцію яка обчислює та повертає площу кола з радіусом r
-function areaCircle(r){
-    let area = Math.PI * Math.pow(r, 2);
-    area = parseFloat(area.toFixed(3));
-    return area;
-}
-rad=parseInt(prompt('Введіть радіус кола:'));
-let areaa=areaCircle(rad)
-if (areaa>0){
-    console.log(`Площа кола радіусом ${rad} = ${areaa}`)
-}
-else {
-    console.log(`Площу кола радіусом ${rad} неможливо обрахувати`)
+//- створити функцію яка обчислює та повертає площу кола з радіусом r
+const areaCircle = (r) => Math.PI * Math.pow(r, 2);
+let rad = parseInt(prompt('Введіть радіус кола:'));
+const areaa = areaCircle(rad);
+
+if (areaa > 0) {
+    console.log(`Площа кола радіусом ${rad} = ${areaa}`);
+} else {
+    console.log(`Площу кола радіусом ${rad} неможливо обрахувати`);
 }
 //- створити функцію яка обчислює та повертає площу циліндру висотою h, та радіутом r
-function cylinderArea(h,r) {
-    return 2*Math.PI*r*h;
-}
-h=parseInt(prompt('Введіть радіус кола:'));
-r=parseInt(prompt('Введіть радіус кола:'));
-let area3=cylinderArea(h,r)
-if (area3>0){
-    console.log(`Бічна площа поверхні циліндра радіусом ${rad} та висотою ${h} = ${area3}`)
-}
-else {
-    console.log(`Бічну площу поверхні циліндра радіусом ${rad} та висотою ${h} неможливо обрахувати`)
+const cylinderArea = (h, r) => 2 * Math.PI * r * h;
+const h = parseInt(prompt('Введіть висоту циліндра:'));
+const r = parseInt(prompt('Введіть радіус кола:'));
+const area3 = cylinderArea(h, r);
+
+if (area3 > 0) {
+    console.log(`Бічна площа поверхні циліндра радіусом ${r} та висотою ${h} = ${area3}`);
+} else {
+    console.log(`Бічну площу поверхні циліндра радіусом ${r} та висотою ${h} неможливо обрахувати`);
 }
 //- створити функцію яка приймає масив та виводить кожен його елемент
-function printArray(arr) {
+const printArray = (arr) => {
     for (let i = 0; i < arr.length; i++) {
         console.log(arr[i]);
     }
 }
-let arr = [3,6,8,0,-6];
+const arr = [3,6,8,0,-6];
 console.log("Елементи масиву:");
 printArray(arr);
 //- створити функцію яка створює параграф з текстом. Текст задати через аргумент
-function paragraph(p) {
+const paragraph = (p) => {
     document.write('<p>' + p + '</p>');
 }
-let Text = prompt("Введіть текст для нового параграфу:");
+const Text = prompt("Введіть текст для нового параграфу:");
 if (Text !== null && Text !== "") {
     paragraph(Text);
 } else {
     alert("Ви не ввели текст. Параграф не створено.");
 }
-
 //- створити функцію яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий
-function list1(text){
-    document.write(`<ul>
+const list1 = (text) => {document.write(`<ul>
 <li>${text}</li>
 <li>${text}</li>
 <li>${text}</li>
@@ -68,7 +60,7 @@ function list1(text){
 }
 list1('Hi!')
 //- створити функцію яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий. Кількість li визначається другим аргументом, який є числовим (тут використовувати цикл)
-function list2(text,n) {
+const list2 = (text,n) => {
     document.write(`<ul>`);
     for (let i=0;i<n;i++){
         document.write(`<li>${text}</li> `) ;
@@ -76,7 +68,7 @@ function list2(text,n) {
 }
 list2('OWU',5)
 //- створити функцію яка приймає масив примітивних елементів (числа,стрінги,булеві), та будує для них список
-function list3(arr) {
+const list3 = (arr) => {
     document.write('<ul>');
     for (let i = 0; i < arr.length; i++) {
         document.write(`<li>${arr[i]}</li>`);
@@ -85,9 +77,9 @@ function list3(arr) {
 }
 let arr7 = [0,'Hi',true, -10];
 list3(arr7);
-
 //- створити функцію яка приймає масив об'єктів з наступними полями id,name,age , та виводить їх в документ. Для кожного об'єкту окремий блок.
-function display(obj) {
+
+const display = (obj) => {
     for (let i = 0; i < obj.length; i++) {
         document.write(
             `<div><p>ID: ${obj[i].id} </p><br>` +
@@ -96,7 +88,6 @@ function display(obj) {
         );
     }
 }
-
 let users = [
     { id: 1, name: 'Maria', age: 20 },
     { id: 2, name: 'Ivan', age: 55 },
@@ -104,10 +95,8 @@ let users = [
 ];
 
 display(users)
-
-
 //- створити функцію яка повертає найменьше число з масиву
-function min(arr) {
+const min = (arr) => {
     let minn = arr[0];
     for (let i = 0; i < arr.length; i++) {
         if (arr[i] < minn) {
@@ -116,40 +105,33 @@ function min(arr) {
     }
     return minn;
 }
-
 let arr4 = [3, -6, 0, 5, 20];
-let minNumber = min(arr4);
+const minNumber = min(arr4);
 console.log(`Мінімальне число масиву ${arr4} є ${minNumber}`);
-
 //- створити функцію sum(arr)яка приймає масив чисел, сумує значення елементів масиву та повертає його. Приклад sum([1,2,10]) //->13
-function sum(arr) {
+const sum = (arr) => {
     let sum=0;
     for (let i = 0; i < arr.length; i++) {
-        sum=sum+arr[i];
+        sum+=arr[i];
     }
     return sum;
 }
-let arr5=[5,8,-30,60,6]
+let arr5=[5,8,-30,60,6];
 let Summa=sum(arr5);
 console.log(`Сума елементів масиву ${arr5} це: ${Summa}`);
-
 //- створити функцію swap(arr,index1,index2). Функція міняє місцями заняення у відаовідних індексах
-function swap(arr, index1, index2) {
+const swap = (arr, index1, index2) => {
     let temporary = arr[index1];
     arr[index1] = arr[index2];
     arr[index2] = temporary;
     return arr;
 }
-
 let arr6 = [3, 6, 7, 8, 2];
-let swap1 = swap(arr6, 2, 3);
-console.log(`Масив ${arr6} з поміняними місцями за індексами 2 та 4: ${swap1}`);
-
-
-
+const swap1 = swap(arr6, 0, 1);
+console.log(`Масив ${arr6} з поміняними місцями за індексами 0 та 1: ${swap1}`);
 //- Написати функцію обміну валюти exchange(sumUAH,currencyValues,exchangeCurrency)
 //Приклад exchange(10000,[{currency:'USD',value:40},{currency:'EUR',value:42}],'USD') // => 250
-function exchange(sumUAH, currencyValues, exchangeCurrency) {
+const exchange = (sumUAH, currencyValues, exchangeCurrency) => {
     let exchangeRate;
     for (let i = 0; i < currencyValues.length; i++) {
         if (currencyValues[i].currency === exchangeCurrency) {
@@ -157,8 +139,10 @@ function exchange(sumUAH, currencyValues, exchangeCurrency) {
             break;
         }
     }
-    let sumInCurrency = sumUAH / exchangeRate;
+    let sumInCurrency;
+    sumInCurrency = sumUAH / exchangeRate;
     return sumInCurrency;
 }
 let result = exchange(5000, [{ currency: 'USD', value: 40 }, { currency: 'EUR', value: 42 }], 'USD');
 console.log(`Сума в обраній валюті: ${result}`);
+
